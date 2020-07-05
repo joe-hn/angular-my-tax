31c36b2eef060b9538a8c7a56f33f48b98649f4f
Jose Orlando Escobar <45208259+joe-hn@users.noreply.github.com>
2020-07-05 11:54:27 -0600

    PRESENTACION

--- src/app/_global.ts
+++ src/app/_global.ts
@@ -1,7 +1,7 @@
 export var _global = {
     /*url: 'http://10.30.5.2:5501/api/',*/
-    url: 'http://localhost:5555/api/', 
+    /* url: 'http://localhost:5555/api/', */
     /*url: 'https://api.mytax.ml/api/',*/
-    /*url: 'https://api.testdev.ml/api/',*/
+    url: 'https://api.testdev.ml/api/',
     date_sql: 'YYYYMMDD'
 }
